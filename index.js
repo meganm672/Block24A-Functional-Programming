982ea7e91d803dcf2b61a9a6c9c3d7f541c6406b
meganm672 <meganm672@gmail.com>
2023-09-09 21:32:37 -0400

    tried the curriying funciton

--- index.js
+++ index.js
@@ -67,9 +67,52 @@ console.log(moreThanEleven(animals));
 
 // Create a function using a callback that takes a string as input and returns the string in reverse order.
 
+//callback function
+const myCallback = (arr) => {
+    return arr.map(item => item.split('').reverse().join(''));
+}
+//function that calls the reverse string on animals array
+function reverseArray(arr, callback) {
+ return callback(arr);
+}
+
+const reverse = reverseArray(animals, myCallback);
+console.log(reverse);
+
+// Create two functions. One function generates runners with specific speeds, 
+// and the other simulates a race between two runners over a specified distance. 
+// Return the winner's name or 'It's a tie!' if both runners finish simultaneously.
+
+const runnersSpeeds = (speed,name) => {
+
+}
+
+const runnerRace = (runner1,runner2,raceDistance) => {
+
+}
 
 //currying function
 
 // Create a function that takes the protocol, subdomain, domain, top-level domain (TLD),
 //  and endpoint as input parameters and returns a concatenated URL string in the format. 
 //  The function should be implemented as a curried function that returns a new function for each input parameter.
+
+const concatenatedUrl = (protocol) => {
+    const protocol = 'https://';
+    const sub = (subdomain)=> {
+        const subdomain = 'www.'
+        const domain = (domains) =>{
+            const domains = 'fullstackacademy.'
+            const topLevelDomain = (TLD) => {
+                const TLD = 'com'
+                const endPoint = (endPoints) => {
+                    const endPoints = '/weather'
+                    return protocol+ subdomain + domains  + TLD + endPoints;
+                };
+            };
+        };
+    };
+}
+
+const weathers = concatenatedUrl(sub)(domain)(fullstackacademy)(com)(weather);
+console.log(weathers);
